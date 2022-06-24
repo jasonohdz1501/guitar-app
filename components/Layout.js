962265c523f665f8a12dba3f1277f92800a03984
jasonohdz1501 <jeissonolartehernandez@gmail.com>
2022-06-24 17:08:05 -0500

    env variables and format font

--- components/Layout.js
+++ components/Layout.js
@@ -11,10 +11,6 @@ const Layout = ({ children, page, starProduct }) => {
         <link rel='icon' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap'
-          rel='stylesheet'
-        />
       </Head>
       <Header starProduct={starProduct} />
       {children}
